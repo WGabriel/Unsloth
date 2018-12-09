5a76a1786456afe8cf9550961daaf7b9811d9921
WGabriel <gabel.werner@gmail.com>
2018-12-09 08:04:44 +0100

    aufgabenbeschreibung fertig

--- index.js
+++ index.js
@@ -57,30 +57,33 @@ app.post('/evaluateFace', function(req, res) {
         jsonObject = JSON.parse(body);
         if (jsonObject[0]["faceAttributes"]["emotion"]["happiness"] > 0.7) {
             userData ={"slothiness" :{"text":"Awesome", "className":"c-btn--success"},
-                "Tasks": {"1":{"prioClass": "c-task--danger", "caption": "Case Study schreiben", "type": "Universität", "date": "10.12.2018"},
-                    "2":{"prioClass": "c-task--danger", "caption": "Einladungsschreiben verschicken", "type": "Winterball", "date": "10.12.2018"},
+                "Tasks": {"1":{"prioClass": "c-task--success", "caption": "Case Study schreiben", "type": "Universität", "date": "10.12.2018"},
+                    "2":{"prioClass": "c-task--success", "caption": "Einladungsschreiben verschicken", "type": "Winterball", "date": "10.12.2018"},
                     "3": {"prioClass": "c-task--warning", "caption": "Programmieraufgabe", "type": "Praktische Übung", "date": "11.12.2018"},
                     "4": {"prioClass": "c-task--warning", "caption": "Übungsblatt Mathe Tutorium", "type": "Universität", "date": "12.12.2018"},
-                    "5": {"prioClass": "c-task--success", "caption": "Zusammenfassung Intercultural Business Communication", "type": "Universität", "date": "13.12.2018"},
-                    "6": {"prioClass": "c-task--success", "caption": "Fall zur Einkommensteuer", "type": "Universität", "date": "15.12.2018"}}};
+                    "5": {"prioClass": "c-task--danger", "caption": "Zusammenfassung Intercultural Business Communication", "type": "Universität", "date": "13.12.2018"},
+                    "6": {"prioClass": "c-task--danger", "caption": "Fall zur Einkommensteuer", "type": "Universität", "date": "15.12.2018"}}};
         } else if (jsonObject[0]["faceAttributes"]["emotion"]["neutral"] > 0.7){
             userData ={"slothiness" :{"text":"Potential", "className":"c-btn--warning"},
-                "Tasks": {"1":{"prioClass": "c-task--danger", "caption": "TEST1", "type": "Übungsblatt", "date": "01.01.2019"},
-                    "2":{"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "02.01.2019"},
-                    "3": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "03.01.2019"},
-                    "4": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "04.01.2019"},
-                    "5": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "05.01.2019"},
-                    "6": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "06.01.2019"}}};
+            "Tasks": {
+              "1": {"prioClass": "c-task--success", "caption": "Übungsblatt Mathe Tutorium", "type": "Universität", "date": "12.12.2018"},
+              "2": {"prioClass": "c-task--success", "caption": "Case Study schreiben", "type": "Universität", "date": "10.12.2018"},
+              "3": {"prioClass": "c-task--warning", "caption": "Programmieraufgabe", "type": "Praktische Übung", "date": "11.12.2018"},
+              "4": {"prioClass": "c-task--warning", "caption": "Fall zur Einkommensteuer", "type": "Universität", "date": "15.12.2018"},
+              "5": {"prioClass": "c-task--danger", "caption": "Einladungsschreiben verschicken", "type": "Winterball", "date": "10.12.2018"},
+              "6": {"prioClass": "c-task--danger", "caption": "Zusammenfassung Intercultural Business Communication", "type": "Universität", "date": "13.12.2018"},
+              }};
         } else {
           userData ={"slothiness" :{"text":"Danger", "className":"c-btn--danger"},
-              "Tasks": {"1":{"prioClass": "c-task--danger", "caption": "TEST2", "type": "Übungsblatt", "date": "01.01.2019"},
-                  "2":{"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "02.01.2019"},
-                  "3": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "03.01.2019"},
-                  "4": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "04.01.2019"},
-                  "5": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "05.01.2019"},
-                  "6": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "06.01.2019"}}};
-        }
-
+          "Tasks": {
+            "4": {"prioClass": "c-task--warning", "caption": "Fall zur Einkommensteuer", "type": "Universität", "date": "15.12.2018"},
+            "5": {"prioClass": "c-task--danger", "caption": "Einladungsschreiben verschicken", "type": "Winterball", "date": "10.12.2018"},
+            "3": {"prioClass": "c-task--warning", "caption": "Programmieraufgabe", "type": "Praktische Übung", "date": "11.12.2018"},
+            "1": {"prioClass": "c-task--success", "caption": "Übungsblatt Mathe Tutorium", "type": "Universität", "date": "12.12.2018"},
+            "6": {"prioClass": "c-task--danger", "caption": "Zusammenfassung Intercultural Business Communication", "type": "Universität", "date": "13.12.2018"},
+            "2": {"prioClass": "c-task--success", "caption": "Case Study schreiben", "type": "Universität", "date": "10.12.2018"},
+            }};
+          }
         console.log(JSON.stringify(jsonObject[0]["faceAttributes"]["emotion"]));
         res.send();
     });
